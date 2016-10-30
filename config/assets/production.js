96dd77e33ca27c97ca5d89adb7a85701ed25e788
UberJavaScript <html.css.jquery@mail.ru>
2016-10-30 22:18:29 +0700

    Unstabile version 1.0

--- config/assets/production.js
+++ config/assets/production.js
@@ -5,31 +5,37 @@
 module.exports = {
   client: {
     lib: {
-      css: [
-        // bower:css
-        'public/lib/bootstrap/dist/css/bootstrap.min.css',
-        'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
-        'public/lib/ng-img-crop/compile/minified/ng-img-crop.css',
-        'public/lib/angular-ui-notification/dist/angular-ui-notification.min.css'
+        css: [
+            // bower:css
+            'public/lib/bootstrap/dist/css/bootstrap.css',
+            'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+            'public/lib/ng-img-crop/compile/unminified/ng-img-crop.css',
+            'public/lib/angular-ui-notification/dist/angular-ui-notification.css',
+            'public/lib/components-font-awesome/css/font-awesome.min.css'
+            // endbower
+        ],
+        js: [
+            // bower:js
+            'public/lib/angular/angular.js',
+            'public/lib/angular-animate/angular-animate.js',
+            'public/lib/angular-sanitize/angular-sanitize.min.js',
+            'public/lib/angular-touch/angular-touch.min.js',
+            'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
+            'public/lib/ng-file-upload/ng-file-upload.js',
+            'public/lib/ng-img-crop/compile/unminified/ng-img-crop.js',
+            'public/lib/angular-messages/angular-messages.js',
+            'public/lib/angular-mocks/angular-mocks.js',
+            'public/lib/angular-resource/angular-resource.js',
+            'public/lib/angular-ui-notification/dist/angular-ui-notification.js',
+            'public/lib/angular-ui-router/release/angular-ui-router.js',
+            'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
+            'public/lib/jquery/dist/jquery.min.js',
+            'public/lib/own/own-jqyery.js'
+            // endbower
+        ]
         // endbower
-      ],
-      js: [
-        // bower:js
-        'public/lib/angular/angular.min.js',
-        'public/lib/angular-animate/angular-animate.min.js',
-        'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
-        'public/lib/angular-messages/angular-messages.min.js',
-        'public/lib/angular-mocks/angular-mocks.js',
-        'public/lib/angular-resource/angular-resource.min.js',
-        'public/lib/angular-ui-notification/dist/angular-ui-notification.min.js',
-        'public/lib/angular-ui-router/release/angular-ui-router.min.js',
-        'public/lib/ng-file-upload/ng-file-upload.min.js',
-        'public/lib/ng-img-crop/compile/minified/ng-img-crop.js',
-        'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
-        // endbower
-      ]
-    },
-    css: 'public/dist/application*.min.css',
-    js: 'public/dist/application*.min.js'
+    }
+    // css: 'public/dist/application*.min.css',
+    // js: 'public/dist/application*.min.js'
   }
 };
