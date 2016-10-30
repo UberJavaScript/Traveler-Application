0013c0b4d6ea705b945ffb7d1b301c10fdf74713
UberJavaScript <html.css.jquery@mail.ru>
2016-10-30 22:49:44 +0700

    Starting point but Articals is steel a problem +mLab URI v1.4

--- config/env/production.js
+++ config/env/production.js
@@ -3,12 +3,12 @@
 var fs = require('fs');
 
 module.exports = {
-  secure: {
-    ssl: true,
-    privateKey: './config/sslcerts/key.pem',
-    certificate: './config/sslcerts/cert.pem',
-    caBundle: './config/sslcerts/cabundle.crt'
-  },
+  // secure: {
+  //   ssl: true,
+  //   privateKey: './config/sslcerts/key.pem',
+  //   certificate: './config/sslcerts/cert.pem',
+  //   caBundle: './config/sslcerts/cabundle.crt'
+  // },
   port: process.env.PORT || 8443,
   // Binding to 127.0.0.1 is safer in production.
   host: process.env.HOST || '0.0.0.0',
